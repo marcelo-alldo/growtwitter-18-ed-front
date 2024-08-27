0a7a5a446dc97ad57f5d95e351f705b60db15d1d
Hugo Amadio <hugo_amadio@hotmail.com>
2024-08-27 20:03:50 -0300

    Fixed - AppRoutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,59 +1,27 @@
-<<<<<<< HEAD
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Profile from "../pages/Profile";
 import Home from "../pages/Home";
 import Error404 from "../pages/Error404";
-=======
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Error404 from '../pages/Error404';
-import CreateAccount from '../pages/CreateAccount';
->>>>>>> development
+import Login from "../pages/Login";
+import CreateAccount from "../pages/CreateAccount";
 
 const router = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   // element: <Login />,
-  //   // errorElement: <Error404 />,
-  // },
-  {
-<<<<<<< HEAD
-    path: "/profile",
-    element: <Profile />,
-    // errorElement: <Error404 />,  
-    // path: "/login",
-    // element: <Login />,
-    errorElement: <Error404 />,
-  },
-  // {
-  //   path: "/perfil",
-  //   // element: <Perfil />,
-  //   errorElement: <Error404 />,
-  // },
-=======
-    path: '/login',
-    element: <Login />,
-    errorElement: <Error404 />,
-  },
-<<<<<<< HEAD
-  {
-    path: '/perfil',
-    element: <Perfil />,
-    errorElement: <Error404 />,
-  },
-=======
-
->>>>>>> feature/add-layout-main
->>>>>>> development
   {
     path: '/',
     element: <Home />,
+    errorElement: <Error404/>
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
   },
   {
     path: '/create-account',
     element: <CreateAccount />,
-    errorElement: <Error404 />,
   },
 ]);
 
