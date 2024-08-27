ede1b6368a18e64d8a21b6d4b7e5407a3d3fe8f1
Hugo Amadio <hugo_amadio@hotmail.com>
2024-08-26 21:38:46 -0300

    added profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,21 @@
 function Profile() {
-  return <h1>profile</h1>;
+  return (
+    <div style={{width: "60vw", height: "100vh", backgroundColor: "#fff"}}>
+        <div style={{width:"100%", height: "30%", backgroundColor: "#cf2222"}}>
+            <div style={{width: '100%', height: "30%", backgroundColor: "#3ee174a5"}}>
+                <div style={{width: "10%", height: "100%", backgroundColor: "#fff"}}>
+
+                </div>
+                <div style={{}}>
+
+                </div>
+            </div>
+        </div>
+        <div style={{width:"100%", height: "70%", backgroundColor: "#cf22225e"}}>
+            Tweets
+        </div>
+    </div>
+  )
 }
 
 export default Profile;
