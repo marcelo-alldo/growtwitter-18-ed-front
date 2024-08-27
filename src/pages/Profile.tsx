70dd1061d52b7d52de3441a121eadbf3db6e772b
Hugo Amadio <hugo_amadio@hotmail.com>
2024-08-27 19:53:24 -0300

    Configured default layout

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,95 +1,101 @@
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ButtonReturn from "../components/ButtonReturn/ButtonReturn";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ButtonReturn from '../components/ButtonReturn/ButtonReturn';
+import DefaultLayout from '../config/layout/DefaultLayout';
 
 function Profile() {
+  const config = {
+    navigation: true,
+    footer: true,
+  };
+
   return (
-    <div
-      style={{
-        width: "60vw",
-        height: "100vh",
-        backgroundColor: "#fff",
-        color: "black",
-        border: "0.5px solid #DCDCDC",
-      }}
-    >
+    <DefaultLayout config={config}>
       <div
         style={{
-          width: "100%",
-          height: "30%",
-          boxSizing: "border-box",
-          padding: "5px",
-          borderBottom: "0.5px solid #DCDCDC",
+          width: '60vw',
+          height: '100vh',
+          backgroundColor: '#fff',
+          color: 'black',
+          border: '0.5px solid #DCDCDC',
         }}
       >
         <div
           style={{
-            width: "100%",
-            height: "30%",
-            display: "flex",
+            width: '100%',
+            height: '30%',
+            boxSizing: 'border-box',
+            padding: '5px',
+            borderBottom: '0.5px solid #DCDCDC',
           }}
         >
           <div
             style={{
-              width: "5%",
-              height: "100%",
-              display: "flex",
-              justifyContent: 'start',
-              alignItems: 'center'
+              width: '100%',
+              height: '30%',
+              display: 'flex',
             }}
           >
-            <ButtonReturn>
-              <ArrowBackIcon htmlColor="#000000" />
-            </ButtonReturn>
-          </div>
-          <div
-            style={{
-              width: "95%",
-              height: "100%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "start",
-              flexDirection: "column",
-            }}
-          >
-            <p style={{ fontSize: "18px", fontWeight: "700", margin: "0px" }}>
-              Perfil de @daphne
-            </p>
-            <p
+            <div
               style={{
-                fontSize: "14px",
-                fontWeight: "500",
-                color: "#848484",
-                margin: "0px",
+                width: '5%',
+                height: '100%',
+                display: 'flex',
+                justifyContent: 'start',
+                alignItems: 'center',
               }}
             >
-              X tweets
-            </p>
+              <ButtonReturn>
+                <ArrowBackIcon htmlColor="#000000" />
+              </ButtonReturn>
+            </div>
+            <div
+              style={{
+                width: '95%',
+                height: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'start',
+                flexDirection: 'column',
+              }}
+            >
+              <p style={{ fontSize: '18px', fontWeight: '700', margin: '0px' }}>Perfil de @daphne</p>
+              <p
+                style={{
+                  fontSize: '14px',
+                  fontWeight: '500',
+                  color: '#848484',
+                  margin: '0px',
+                }}
+              >
+                X tweets
+              </p>
+            </div>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'start',
+            }}
+          >
+            <img
+              src=""
+              alt=""
+              style={{
+                backgroundColor: '#be0a0a',
+                width: '80px',
+                height: '80px',
+                borderRadius: '50%',
+                border: '3px solid gray',
+              }}
+            />
+            <p style={{ margin: '0px', fontWeight: '500' }}>Daphne Dog</p>
+            <p style={{ margin: '0px', fontWeight: '500' }}>@perfil</p>
           </div>
         </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "start",
-          }}
-        >
-          <img
-            src=""
-            alt=""
-            style={{
-              backgroundColor: "#be0a0a",
-              width: "80px",
-              height: "80px",
-              borderRadius: "50%",
-              border: "3px solid gray",
-            }}
-          />
-          <p style={{ margin: "0px", fontWeight:'500' }}>Daphne Dog</p>
-          <p style={{ margin: "0px", fontWeight: '500' }}>@perfil</p>
-        </div>
+        <div style={{ width: '100%', height: '70%' }}>Tweets</div>
       </div>
-      <div style={{ width: "100%", height: "70%" }}>Tweets</div>
-    </div>
+    </DefaultLayout>
   );
 }
 
