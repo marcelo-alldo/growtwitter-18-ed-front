f2321ff6f4028fe84f2e73a28fd09f40669eff43
YuriWirth <yuriabsurdoo15@gmail.com>
2024-08-27 20:38:51 -0300

    feat: add axios, add api endPoints, GET AND POST

---
+++ src/services/api.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+async function doPost(url: string, data: any, token: string) {
+  let auth = false;
+  try {
+    const response = await api.post(url, data, { headers: { Authorization: token } });
+    if (response.status === 200) {
+      auth = true;
+      return { ...response.data, auth };
+    }
+    return { success: false, msg: 'Post Error' };
+  } catch (error) {
+    /*ESSE ERRO NÃO AFETA O CODIGO.*/
+    if ((error?.response?.status = 401)) {
+      auth = false;
+    }
+    return { success: false, msg: 'Post Error', auth };
+  }
+}
+async function doGet(url: string, token: string) {
+  let auth = false;
+  try {
+    const response = await api.get(url, {
+      headers: { Authorization: token },
+    });
+    if (response.status === 200) {
+      auth = true;
+      return { ...response.data, auth };
+    }
+    return { success: false, msg: 'Get Error', auth: true };
+  } catch (error) {
+    if ((error?.response?.status = 401)) {
+      auth = false;
+    }
+    return { success: false, msg: 'Get Error', auth };
+  }
+}
+
+export default { doPost, doGet };
