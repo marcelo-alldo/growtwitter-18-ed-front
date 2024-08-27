95b62efa6d36182124f7babdfbfd38d152546467
Edsamn <eduardomsribas@gmail.comm>
2024-08-27 19:16:25 -0300

    feat: add pagina de criação de conta

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,22 +1,28 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Error404 from "../pages/Error404";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Error404 from '../pages/Error404';
+import CreateAccount from '../pages/CreateAccount';
 
 const router = createBrowserRouter([
   {
-    path: "/login",
+    path: '/login',
     element: <Login />,
     errorElement: <Error404 />,
   },
   {
-    path: "/perfil",
+    path: '/perfil',
     element: <Perfil />,
     errorElement: <Error404 />,
   },
   {
-    path: "/",
+    path: '/',
     element: <Home />,
+  },
+  {
+    path: '/create-account',
+    element: <CreateAccount />,
+    errorElement: <Error404 />,
   },
 ]);
 
