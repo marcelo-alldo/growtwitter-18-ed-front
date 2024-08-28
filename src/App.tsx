0da2fbce11461626836d7450f00106c21894539a
Marcelo Eltz <marcelo@growdev.com.br>
2024-08-28 19:13:11 -0300

    ajuste merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import DefaultTheme from "./config/theme/DefaultTheme";
-import AppRoutes from "./routes/AppRoutes";
-import '../src/App.css';
-=======
 import DefaultTheme from './config/theme/DefaultTheme';
 import AppRoutes from './routes/AppRoutes';
->>>>>>> f4b6cd5cfe537520c8c408a039f24fbd0dc4df6e
+import '../src/App.css';
 
 function App() {
   return (
