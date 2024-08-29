0f167982e0ca64f74712d43d57645333c6944b83
Edsamn <eduardomsribas@gmail.comm>
2024-08-29 13:10:34 -0300

    feat: add criação de conta

--- src/pages/CreateAccount.tsx
+++ src/pages/CreateAccount.tsx
@@ -1,24 +1,24 @@
 import { useState } from 'react';
 import H2Styled from '../components/login/H2Styled';
 import LabelStyled from '../components/login/LabelStyled';
-import LoginButtonDefault from '../components/login/LoginButtonDefault';
 import LoginInputStyled from '../components/login/LoginInputStyled';
 import { useNavigate } from 'react-router-dom';
 import { doPost } from '../services/api';
+import ButtonDefault from '../components/botao/ButtonDefault';
 
 function CreateAccount() {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [name, setName] = useState<string>('');
-  const [userName, setUserName] = useState<string>('');
+  const [username, setUsername] = useState<string>('');
   const navigate = useNavigate();
 
   async function handleCreateAccount() {
-    if (!email || !password || !name || !userName) {
+    if (!email || !password || !name || !username) {
       alert('Favor preencher os campos.');
     }
 
-    const response = await doPost('/users', { email, password, name, userName }, '');
+    const response = await doPost('/users', { email, password, name, username }, '');
     console.log(response);
 
     if (response.success) {
@@ -82,13 +82,13 @@ function CreateAccount() {
             <LabelStyled htmlFor="name">Nome</LabelStyled>
             <LoginInputStyled type="text" value={name} onChange={ev => setName(ev.target.value)} />
             <LabelStyled htmlFor="userName">Apelido</LabelStyled>
-            <LoginInputStyled type="text" value={userName} onChange={ev => setUserName(ev.target.value)} />
+            <LoginInputStyled type="text" value={username} onChange={ev => setUsername(ev.target.value)} />
             <LabelStyled htmlFor="email">Email</LabelStyled>
             <LoginInputStyled type="text" value={email} onChange={ev => setEmail(ev.target.value)} />
             <LabelStyled htmlFor="password">Senha</LabelStyled>
             <LoginInputStyled type="password" value={password} onChange={ev => setPassword(ev.target.value)} />
           </div>
-          <LoginButtonDefault label="Entrar" action={handleCreateAccount} />
+          <ButtonDefault label="Entrar" action={handleCreateAccount} />
         </div>
       </div>
     </div>
