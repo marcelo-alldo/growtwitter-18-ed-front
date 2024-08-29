94366274cf5cab0d28c17d821100d56f19080ef8
DionataRech <dionata.rech@gmail.com>
2024-08-28 22:47:14 -0300

    feat: Add Explore , Explore Routes , Componente ExploreStyled

---
+++ src/components/explore/ExploreStyled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ExploreStyled = styled.div`
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export default ExploreStyled;
