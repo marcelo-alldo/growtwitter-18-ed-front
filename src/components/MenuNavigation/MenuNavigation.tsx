7b2fa64549b9a7f2f5de85c8c931a181486a0a08
DionataRech <dionata.rech@gmail.com>
2024-08-29 21:00:32 -0300

    add modal

--- src/components/MenuNavigation/MenuNavigation.tsx
+++ src/components/MenuNavigation/MenuNavigation.tsx
@@ -3,8 +3,31 @@ import iconPageInitial from '../../../public/icone_pagina inicial_selecionado.sv
 import iconPageExplorer from '../../../public/icone_explorar_selecionado.svg';
 import iconPageProfile from '../../../public/icone_perfil.svg';
 import ButtonDefault from '../button/ButtonDefault';
+import Modal from '../modal/Modal';
+import { useState } from 'react';
+import { doPost } from '../../services/api';
 
 function MenuNavigation() {
+  const [show, setShow] = useState<boolean>(false);
+  const [value, setValue] = useState<string>('');
+  function showModal() {
+    setShow(!show);
+  }
+  async function sendTweet(value: string) {
+    const getAuth = JSON.stringify(localStorage.getItem('userLogged'));
+
+    try {
+      const response = await doPost('/tweet', value, getAuth);
+      if (response.success) {
+        alert(response.msg);
+        setValue('');
+        setShow(false);
+      }
+    } catch (error) {
+      return error;
+    }
+  }
+
   return (
     <div className="container">
       <div className="menu">
@@ -19,7 +42,17 @@ function MenuNavigation() {
           </nav>
         </div>
         <div>
-          <ButtonDefault label="Tweetar" />
+          {show ? (
+            <Modal
+              actionCancel={showModal}
+              actionConfirm={sendTweet(value)}
+              setValue={e => setValue(e.target.value)}
+              value={value}
+            />
+          ) : (
+            ''
+          )}
+          <ButtonDefault label="Tweetar" action={showModal} />
         </div>
       </div>
 
