cedfbc4a4581dff51bc368b88a1338c1d62e64cb
RafaelHenkel <rafael0henkel1@gmail.com>
2024-09-03 20:29:20 -0300

    fix: add file `vite.config.ts`

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,19 +1,19 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Profile from '../pages/Profile';
 import Home from '../pages/Home';
 import Error404 from '../pages/Error404';
 import Login from '../pages/Login';
 import CreateAccount from '../pages/CreateAccount';
 import Explore from '../pages/Explore';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
-    // errorElement: <Error404 />,
+    errorElement: <Error404 />,
   },
   {
-    path: "/profile",
+    path: '/profile',
     element: <Profile />,
   },
   {
