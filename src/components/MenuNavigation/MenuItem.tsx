7e30c85e657afee67a1e4c1366ff08c5767e9377
MateusCamargoS-1 <mateuscamargodasilva2@gmail.com>
2024-09-02 22:40:28 -0300

    add-layout-menu-ativo

--- src/components/MenuNavigation/MenuItem.tsx
+++ src/components/MenuNavigation/MenuItem.tsx
@@ -1,19 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import MenuItemStyles from './MenuItemStyles';
 
 
 interface MenuItemProps {
   icon: string;
+  iconActive: string;
   label: string;
   route: string;
 }
 
-function MenuItem({ icon, label, route }: MenuItemProps) {
+function MenuItem({ icon, iconActive, label, route }: MenuItemProps) {
+  const location = useLocation();
+  const active = location.pathname === route;
   return (
     <Link to={route}>
       <MenuItemStyles>
         <div className='icon-Menu'>
-          <img src={icon}></img>
+          <img src={active ? iconActive : icon}></img>
         </div>
         <div className='label-Menu'>{label}</div>
         </MenuItemStyles>
