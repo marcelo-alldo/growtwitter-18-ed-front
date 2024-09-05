dc25bd334f70a6fec7a1de4e2d763696ca7f2fdc
YuriWirth <yuriabsurdoo15@gmail.com>
2024-09-04 21:33:16 -0300

    feat: loading central page

--- src/config/layout/DefaultLayout.tsx
+++ src/config/layout/DefaultLayout.tsx
@@ -1,10 +1,11 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import DefaultLayoutStyled from './DefaultLayoutStyled';
 import HeaderStyled from '../../components/HeaderStyled';
 import MainStyled from '../../components/MainStyled';
 import FooterStyled from '../../components/FooterStyled';
 import MenuNavigation from '../../components/MenuNavigation/MenuNavigation';
 import CardAssunto from '../../components/CardAssuntos/CardAssunto';
+import { CircularProgress } from '@mui/material';
 
 interface DefaultLayoutProps {
   children: ReactNode;
@@ -15,12 +16,16 @@ interface DefaultLayoutProps {
 }
 
 function DefaultLayout({ children }: DefaultLayoutProps) {
+  const [loading, setLoading] = useState<boolean>(true);
+  setTimeout(() => {
+    setLoading(false);
+  }, 3000);
   return (
     <DefaultLayoutStyled>
       <HeaderStyled>
         <MenuNavigation />
       </HeaderStyled>
-      <MainStyled>{children}</MainStyled>
+      {loading ? <CircularProgress /> : <MainStyled>{children}</MainStyled>}
       <FooterStyled>
         <CardAssunto />
       </FooterStyled>
